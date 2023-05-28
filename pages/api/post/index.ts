4d28dbfc14364b9bf3a1730199a416e86fbc90a4
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-05-28 16:02:22 +1000

    add new and display draft

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -2,14 +2,16 @@
 
 import { getSession } from 'next-auth/react';
 import prisma from '../../../lib/prisma';
+import {options} from '../auth/[...nextauth]';
+import { getServerSession } from 'next-auth';
 
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
 export default async function handle(req, res) {
   const { title, content } = req.body;
-
-  const session = await getSession({ req });
+  const authOptions = options;
+  const session = await getServerSession(req, res, authOptions);
   const result = await prisma.post.create({
     data: {
       title: title,
